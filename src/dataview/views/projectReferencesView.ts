11abe425780f1c934fb4f0c70a01cbaea354aa40
stone <github@stone-bo.com>
2025-07-22 22:35:04 +0100

    refactor(dataview): add an extra category column and sort by it

--- src/dataview/views/projectReferencesView.ts
+++ src/dataview/views/projectReferencesView.ts
@@ -19,13 +19,25 @@ const pages = dv.pages(refFolder).filter(refPage => {
     return refPage.sources.some(source => currentPage.sources.some(cSource => cSource.path === source.path));
 });
 
-console.error(pages)
+const catTags = [
+    "#research/background",
+    "#research/problem",
+    "#research/hypothesis",
+    "#research/method",
+    "#research/experiment",
+    "#vocabulary",
+    "#research/personal-points",
+    "#research/other"
+]
+
 dv.table(
-    ["Description", "Note Date"],
-    pages.map(p => [
-        \`[[\${p.file.name}|📍]]\${p.DisplayName}\`,
-        p.year
-    ])
+    ["Description", "Cat", "Note Date"],
+    pages.map(p => {
+        const desc = \`[[\${p.file.name}|📍]]\${p.DisplayName}\`
+        const cat = p.file.etags.find(tag => catTags.includes(tag)) || "No Category"
+        const pureCatName = cat.split("/").pop()
+        return [desc, pureCatName, p.year]
+    }).sort(p=> p[1] ? p[1] : "No Date")
 )
 	`,
 };
