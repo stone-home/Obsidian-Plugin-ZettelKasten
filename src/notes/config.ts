086c90e4b230180f573680498d7d320585f69aa6
stone <github@stone-bo.com>
2025-08-04 21:16:48 +0100

    chore: modify the upgrade path of atomic

--- src/notes/config.ts
+++ src/notes/config.ts
@@ -23,7 +23,7 @@ export const NoteTypeData: Record<NoteType, INoteTemplateMetadata> = {
 		emoji: "📚",
 		description: "A note summarizing literature or research findings.",
 		path: "002-literature/001-notes",
-		upgradePath: [NoteType.FLEETING, NoteType.ATOMIC, NoteType.PERMANENT],
+		upgradePath: [NoteType.FLEETING, NoteType.LITERATURE, NoteType.ATOMIC, NoteType.PERMANENT],
 	},
 	[NoteType.ATOMIC]: {
 		label: "Atomic",
@@ -31,7 +31,7 @@ export const NoteTypeData: Record<NoteType, INoteTemplateMetadata> = {
 		description:
 			"A small, self-contained note that can be linked to others.",
 		path: "003-atomic/001-notes",
-		upgradePath: [NoteType.FLEETING, NoteType.PERMANENT],
+		upgradePath: [NoteType.ATOMIC, NoteType.PERMANENT],
 	},
 	[NoteType.PERMANENT]: {
 		label: "Permanent",
